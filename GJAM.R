0cee429e87a7156d43c8398424a4315b5f51e3d0
APasinsky <kaldredpayne@gmail.com>
2023-06-20 14:00:55 -0400

    column error testing

--- GJAM.R
+++ GJAM.R
@@ -39,38 +39,66 @@ print(strain_names)
 
 
 modelList <- list(
-  ng = 2500,  # number of MCMC generations
-  burnin = 500,  # burn-in period
+  ng = 100,  # number of MCMC generations
+  burnin = 5,  # burn-in period
   typeNames = rep("DA", length(strain_names)),  # model type for each response variable
-  reductList = list(r = 8, N = 20)  # dimension reduction parameters
+  REDUCT = FALSE  # dimension reduction parameters
 )
 
 #for (strainName in strain_names) {
-  #print(strainName)
-  
-  # Prepare data
-  yData <- as.matrix(data_wide[, strain_names]) # using the strain_names vector to select response variables
-  # Create xData as a data frame and convert columns to correct data types
-  xData <- cbind(Time = as.numeric(data_wide$Time),
-                 Run = as.numeric(data_wide$Run),
-                 condition = as.numeric(as.factor(data_wide$condition))) # converting factors to numeric
-  
-  # Convert xData to a matrix
-  xData <- as.matrix(xData)
-  
+#print(strainName)
 
-  
-  formula <- as.formula( ~ Time + condition + Run)
-  
-  # Apply GJAM
-  gjamFit <- gjam(formula, xdata = xData, ydata = yData, modelList = modelList)
-  
-  # Check the result
-  print(summary(gjamFit))
-  
-  # Create diagnostic plots
-  par(mfrow = c(2,2))
-  gjamPlot(output = gjamFit, plotPars = list(GRIDPLOTS = TRUE))
-  
- # }
+# Prepare data
+yData <- as.matrix(data_wide[, strain_names]) # using the strain_names vector to select response variables
+# Create xData as a data frame and convert columns to correct data types
+xData <- cbind(Time = as.numeric(data_wide$Time),
+               Run = as.factor(data_wide$Run),
+               condition = as.factor(data_wide$condition)) # converting factors to numeric
+
+# Convert xData to a matrix
+xData <- as.matrix(xData)
+
+
+
+formula <- as.formula( ~ Time + condition + Run)
+
+# Apply GJAM
+gjamFit <- gjam(formula, xdata = xData, ydata = yData, modelList = modelList)
+
+# Check the result
+print(summary(gjamFit))
+
+# Create diagnostic plots
+par(mfrow = c(2,2))
+gjamPlot(output = gjamFit, plotPars = list(GRIDPLOTS = TRUE))
+
+
+
+
+
+newdata <- list(xdata = xData, nsim = 200)
+predictions <- gjamPredict(gjamFit, newdata = newdata)
+
+# Base ggplot
+base <- ggplot()
+
+# make plot for first strain
+strain1 <- base +
+  geom_point(aes(x = yData[,1],  # observed values for first strain
+                 y = predictions$muY[,1])) +  # predicted values for first strain
+  labs(x = "Observed", y = "Predicted", title = paste0(strain_names[1]))
+
+# make plot for second strain
+strain2 <- base +
+  geom_point(aes(x = yData[,2],  # observed values for second strain
+                 y = predictions$muY[,2])) +  # predicted values for second strain
+  labs(x = "Observed", y = "Predicted", title = paste0(strain_names[2]))
+
+# Use the ggarrange function from the ggpubr package to arrange the two plots side by side
+ggarrange(strain1, strain2, ncol = 2)
+
+
+
+
+# }
 
