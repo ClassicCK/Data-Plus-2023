164013ddffef3f405c528f5d9750bb4549982127
AnthonyDu27 <duanthony2004@gmail.com>
2023-06-27 16:11:57 -0400

    Fit with a AR model

--- Recursive_Formula.R
+++ Recursive_Formula.R
@@ -38,17 +38,7 @@ conditions <- list("no glu01","glu01")
 models <- list("Linear","Exponential","Logistic","Gompertz","Richards","Stannard")
 
 #Defines equations of fit
-logistic_equation <- function(time, state, parameters) {
 
-    r = parameters$r
-    K = parameters$K
-    
-    # Define the differential equation
-    dOD <- r * state * (1 - state / K)
-    
-    # Return the derivative
-    return(list(dOD))
-}
 
 
 r_squareds <- data.frame(c(0))
@@ -72,11 +62,11 @@ for (i in strains){ #For each strain:
     print(j)
     strain_runs <- strain_unfiltered %>% filter(strain_unfiltered$condition == j) #selects the strain from strains
     
-    for (k in unique(strain_runs$Run)){
+    for (k in unique(strain_runs$Variable)){
       
       print(k)
       strain <- strain_runs %>% filter(strain_runs$Variable == k)
-    
+      
       strain$Variable <- as.factor(strain$Variable) #factors the Variable column soR does not create a bar graph 
       strain$techrep <- as.factor(strain$techrep)
       strain$biorep <- as.factor(strain$biorep)
@@ -86,40 +76,26 @@ for (i in strains){ #For each strain:
       strain$Time <- as.integer(strain$Time)/(60*60) #Convert to Hours
       #Convert to Hours
       
-      ssq=function(parms){
+      strain[c("Time","OD")]
       
-        initial <- c(strain$OD[1])
-        t <- seq(min(strain$Time), max(strain$Time))
-        
-        parms <- list(r=0.01,K=1)
-        out <- ode(y=initial,times=t,func=logistic_equation,parms=parms)
-        plot(out)
-        
-        outdf=data.frame(out)
-        outdf=outdf[outdf$time %in% strain$Time,]
-        # Evaluate predicted vs experimental residual
-        preddf=melt(outdf,id.var="time",variable.name="Run",value.name="OD")
-        expdf=melt(strain,id.var="Time",variable.name="Run",value.name="OD")
-        
-        preddf <- preddf[complete.cases(preddf), ]  # Remove rows with NAs
-        expdf <- expdf[complete.cases(expdf), ]
-        
-        preddf$OD <- as.numeric(preddf$OD)  # Convert OD column to numeric
-        expdf$OD <- as.numeric(expdf$OD)  # Convert OD column to numeric
-        
-        
-        ssqres=preddf$OD-expdf$OD
-        # return predicted vs experimental residual
-        return(ssqres)
-      }
+
+      AR <- arima(strain$OD, order = c(1,0,0))
+      print(AR)
+      AR_fit <- strain$OD - residuals(AR)
+
+      plot_data <- data.frame(Time = strain$Time, OD = strain$OD, AR_fit = AR_fit)
+      # Convert the "Time" column to numeric (if it's not already)
+      plot_data$Time <- as.numeric(plot_data$Time)
       
-      parms <- c(r=0.01,K=0.5)
-      # fitting
-      fitval <- nls.lm(par=parms,fn=ssq)
-      summary(fitval)
-    
-    
-    
+      # Plot strain and AR_fit using ggplot2
+      strainGraph <- ggplot(plot_data, aes(x = Time)) +
+        geom_line(aes(y = OD, color = "Strain")) +
+        geom_line(aes(y = AR_fit, color = "AR Fit"), linetype = "dashed") +
+        labs(x = "Time", y = "Optical Density", title = paste("Time series plot for strain: ",i, " with ", j, "run:", k, sep="")) + theme_twoseventyeight
+      
+      ggsave(filename = paste("Figures/",i," with ", j, " run: ", k, ".png",sep=""),
+             plot = strainGraph, units = "px", width = 3200, height = 1800, dpi = 300)
+ 
     } 
   }
 }
